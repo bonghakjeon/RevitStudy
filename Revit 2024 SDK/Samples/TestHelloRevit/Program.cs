b7a852d2e5c072e37e9ff2905e104ce6a7878e5e
DESKTOP-GUHF3TT\bhjeon <bhjeon@imbu.co.kr>
2023-08-11 11:52:59 +0900

    Revit2024 SDK 커밋

---
+++ Revit 2024 SDK/Samples/TestHelloRevit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestHelloRevit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
