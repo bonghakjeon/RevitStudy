381ade34837be87b2497d6207b73b523691d3cc6
DESKTOP-GUHF3TT\bhjeon <bhjeon@imbu.co.kr>
2023-11-02 17:56:53 +0900

    ProgressBar 뷰모델 바인딩 할 수 있도록 테스트 코드 구현 (추후 수정 작업 예정)

--- RevitBoxSeumteo/RevitBoxSeumteo/ViewModels/Windows/ParamsCreateVM.cs
+++ RevitBoxSeumteo/RevitBoxSeumteo/ViewModels/Windows/ParamsCreateVM.cs
@@ -1,5 +1,6 @@
 ﻿using Serilog;
 using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -19,7 +20,10 @@ namespace RevitBoxSeumteo.ViewModels.Windows
     // TODO : Background Worker 클래스 사용해서 ProgressBar 화면 뷰(ParamsCreateV.xaml) + 뷰모델(ParamsCreateVM.cs) 구현 (2023.10.30 jbh)
     // 참고 URL - http://ojc.asia/bbs/board.php?bo_table=WPF&wr_id=40
     // 참고 2 URL - https://afsdzvcx123.tistory.com/entry/WPF-WPF-ProgressBar-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0%ED%94%84%EB%A1%9C%EA%B7%B8%EB%A0%88%EC%8A%A4%EB%B0%94
-    public class ParamsCreateVM
+    // 참고 3 URL - https://stackoverflow.com/questions/14262220/wpf-progressbar-with-value-databinding
+    // 참고 4 URL - https://afsdzvcx123.tistory.com/entry/WPF-WPF-ProgressBar-%EC%BB%A8%ED%8A%B8%EB%A1%A4-MVVM-%ED%8C%A8%ED%84%B4%EC%9C%BC%EB%A1%9C-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
+    // 참고 5 URL - https://www.codeproject.com/Questions/5292748/Open-new-progressbar-window-in-WPF-with-MVVM-patte
+    public class ParamsCreateVM :INotifyPropertyChanged
     {
         #region 프로퍼티
 
@@ -55,6 +59,10 @@ namespace RevitBoxSeumteo.ViewModels.Windows
         /// </summary>
         private BackgroundWorker ThreadParamsCreate = null;
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double CurrentProgress { get; set; }
+
         #endregion 프로퍼티
 
         #region 생성자
@@ -68,14 +76,15 @@ namespace RevitBoxSeumteo.ViewModels.Windows
             TxtParamsCreate = AISParamsHelper.Text_매개변수생성클릭;
             BtnParamsCreate = AISParamsHelper.Btn_매개변수생성;
 
+
             // 백그라운드 워커 초기화
             // 작업의 진행율이 바뀔때 ProgressChanged 이벤트 발생여부
             // 작업취소 가능여부 true로 설정
-            ThreadParamsCreate = new BackgroundWorker()
-            {
-                WorkerReportsProgress = true,
-                WorkerSupportsCancellation = true
-            };
+            //ThreadParamsCreate = new BackgroundWorker()
+            //{
+            //    WorkerReportsProgress = true,
+            //    WorkerSupportsCancellation = true
+            //};
 
             // 백그라운드에서 실행될 콜백 이벤트 생성
             // For the performing operation in the background.  
@@ -116,7 +125,26 @@ namespace RevitBoxSeumteo.ViewModels.Windows
 
             try
             {
+                // TODO : ProgressBar 뷰모델에서 구현하기 (2023.11.02 jbh)
+                // 참고 URL - https://afsdzvcx123.tistory.com/entry/WPF-WPF-ProgressBar-%EC%BB%A8%ED%8A%B8%EB%A1%A4-MVVM-%ED%8C%A8%ED%84%B4%EC%9C%BC%EB%A1%9C-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
 
+                ThreadParamsCreate = new BackgroundWorker()
+                {
+                    WorkerReportsProgress = true,
+                    WorkerSupportsCancellation = true,
+                };
+
+                ThreadParamsCreate.DoWork += ParamsCreateDoWork;
+                ThreadParamsCreate.ProgressChanged += ProgressChanged;
+                ThreadParamsCreate.RunWorkerCompleted += RunWorkerCompleted;
+                ThreadParamsCreate.RunWorkerAsync();
+
+                // for (Percentage = 0; Percentage <= 100; Percentage++)
+                // {
+                //     // ThreadParamsCreate.ReportProgress(i);   // 값을 ReportProgress 매개변수로 전달
+                //     // Thread.Sleep(100);                      // 0.1초
+                //     // Wait(100);
+                // }
             }
             catch (Exception e)
             {
@@ -128,6 +156,71 @@ namespace RevitBoxSeumteo.ViewModels.Windows
         }
 
         #endregion ParamsCreateAsync
+
+
+        #region ParamsCreateDoWork
+
+        /// <summary>
+        /// ParamsCreateDoWork 스레드 이벤트 핸들러
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ParamsCreateDoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                // Percentage += 1;
+                Thread.Sleep(100);                      // 0.1초
+                ThreadParamsCreate.ReportProgress(i);   // 값을 ReportProgress 매개변수로 전달
+                
+                // Wait(100);
+            }
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    ThreadParamsCreate.ReportProgress(i);   // 값을 ReportProgress 매개변수로 전달
+            //    // Thread.Sleep(100);                      // 0.1초
+            //    // Wait(100);
+            //}
+
+            // for (Percentage = 0; Percentage <= 100; Percentage++)
+            // {
+            //     // ThreadParamsCreate.ReportProgress(i);   // 값을 ReportProgress 매개변수로 전달
+            //     Thread.Sleep(100);                      // 0.1초
+            //     // Wait(100);
+            // }
+        }
+
+        #endregion ParamsCreateDoWork
+
+        #region ProgressChanged
+
+        private void ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // throw new NotImplementedException();
+            CurrentProgress = e.ProgressPercentage;
+
+            
+
+        }
+
+        #endregion ProgressChanged
+
+        #region 
+
+        /// <summary>
+        /// 프로그래스바 컨트롤 작업 끝났을 때
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+            // throw new NotImplementedException();
+            // Percentage = 100;
+        }
+
+        #endregion 
 
         #region Sample
 
